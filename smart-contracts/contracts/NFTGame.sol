e24045f83a517555f74580c232c9e257180f1b23
WizardlyDeveloper <wizardlydeveloper@gmail.com>
2022-06-20 04:11:15 +0500

    Add dynamic tokenURI for nft

--- smart-contracts/contracts/NFTGame.sol
+++ smart-contracts/contracts/NFTGame.sol
@@ -2,9 +2,18 @@
 
 pragma solidity ^0.8.0;
 
+
+// NFT contract to inherit from.
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// Helper functions OpenZeppelin provides.
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+
 import "hardhat/console.sol";
 
-contract NFTGame {
+contract NFTGame is ERC721{
 
 // We'll hold our character's attributes in a struct. Feel free to add
   // whatever you'd like as an attribute! (ex. defense, crit chance, etc).
@@ -18,19 +27,29 @@ contract NFTGame {
     }
 
 
+    // The tokenId is the NFTs unique identifier, it's just a number that goes
+  // 0, 1, 2, 3, etc.
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+
 // A lil array to help us hold the default data for our characters.
   // This will be helpful when we mint new characters and need to know
   // things like their HP, AD, etc.
     CharacterAttributes[] defaultCharacters;
 
+    // We create a mapping from the nft's tokenId => that NFTs attributes.
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
 
+    // ADDRESS => TokenId, to store the owner of the NFT and reference it later.
+    mapping(address => uint256) public nftHolders;
 
   constructor(
     string[] memory characterNames,
     string[] memory characterImageURIs,
     uint[] memory characterHp,
     uint[] memory characterAttackDmg
-  ) {
+  ) ERC721("SuperHeroes", "SHero") {
     
      // Loop through all the characters, and save their values in our contract so
     // we can use them later when we mint our NFTs.
@@ -52,5 +71,66 @@ contract NFTGame {
 
 
     console.log("THIS IS MY GAME CONTRACT. NICE.");
+    // increment _tokenIds here so that the first NFT has an ID of 1.
+    _tokenIds.increment();
   }
+
+    // Users would be able to hit this function and get their NFT based on the
+    // characterId they send in!
+    function mintCharacterNFT(uint _characterIndex) external {
+    // Get current tokenId (starts at 1 since we incremented in the constructor).
+        uint256 newItemId = _tokenIds.current();
+
+        // The magical function! Assigns the tokenId to the caller's wallet address.
+        _safeMint(msg.sender, newItemId);
+
+        // We map the tokenId => their character attributes. More on this in
+        // the lesson below.
+        nftHolderAttributes[newItemId] = CharacterAttributes({
+        characterIndex: _characterIndex,
+        name: defaultCharacters[_characterIndex].name,
+        imageURI: defaultCharacters[_characterIndex].imageURI,
+        hp: defaultCharacters[_characterIndex].hp,
+        maxHp: defaultCharacters[_characterIndex].maxHp,
+        attackDamage: defaultCharacters[_characterIndex].attackDamage
+        });
+
+        console.log("Minted NFT w/ tokenId %s and characterIndex %s", newItemId, _characterIndex);
+        
+        // Keep an easy way to see who owns what NFT.
+        nftHolders[msg.sender] = newItemId;
+
+        // Increment the tokenId for the next person that uses it.
+        _tokenIds.increment();
+    }
+
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenId];
+
+        string memory strHp = Strings.toString(charAttributes.hp);
+        string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+        string memory strAttackDamage = Strings.toString(charAttributes.attackDamage);
+
+        string memory json = Base64.encode(
+            abi.encodePacked(
+            '{"name": "',
+            charAttributes.name,
+            ' -- NFT #: ',
+            Strings.toString(_tokenId),
+            '", "description": "This is an NFT that lets people play in the game Metaverse Slayer!", "image": "',
+            charAttributes.imageURI,
+            '", "attributes": [ { "trait_type": "Health Points", "value": ',strHp,', "max_value":',strMaxHp,'}, { "trait_type": "Attack Damage", "value": ',
+            strAttackDamage,'} ]}'
+            )
+        );
+
+        string memory output = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+        
+        return output;
+    }
+
+
+
 }
--- smart-contracts/scripts/run.js
+++ smart-contracts/scripts/run.js
@@ -1,15 +1,24 @@
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('NFTGame');
     const gameContract = await gameContractFactory.deploy(
-        ["Leo", "Aang", "Pikachu"],       // Names
-        ["https://i.imgur.com/pKd5Sdk.png", // Images
-            "https://i.imgur.com/xVu4vFL.png",
-            "https://i.imgur.com/WMB6g9u.png"],
-        [100, 200, 300],                    // HP values
-        [100, 50, 25]                       // Attack damage values
+        ["Naruto", "Saitama", "Goku"],       // Names
+        ["https://i.imgur.com/jQ4PZTE.jpeg", // Images
+            "https://i.imgur.com/NBEIFBz.png",
+            "https://i.imgur.com/5gAuNtn.png"],
+        [400, 300, 500],                    // HP values
+        [300, 500, 2500]                       // Attack damage values
     );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
+
+    // We only have three characters.
+    // an NFT w/ the character at index 2 of our array.
+    txn = await gameContract.mintCharacterNFT(2);
+    await txn.wait();
+
+    // Get the value of the NFT's URI.
+    let returnedTokenUri = await gameContract.tokenURI(1);
+    console.log("Token URI:", returnedTokenUri);
 };
 
 const runMain = async () => {
