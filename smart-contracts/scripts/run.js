c19a36d53722b5825df51c754cf6f3680e1de5a6
WizardlyDeveloper <wizardlydeveloper@gmail.com>
2022-06-19 22:57:28 +0500

    update contract -- default characters and script

--- smart-contracts/scripts/run.js
+++ smart-contracts/scripts/run.js
@@ -1,19 +1,25 @@
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('NFTGame');
-    const gameContract = await gameContractFactory.deploy();
+    const gameContract = await gameContractFactory.deploy(
+        ["Leo", "Aang", "Pikachu"],       // Names
+        ["https://i.imgur.com/pKd5Sdk.png", // Images
+            "https://i.imgur.com/xVu4vFL.png",
+            "https://i.imgur.com/WMB6g9u.png"],
+        [100, 200, 300],                    // HP values
+        [100, 50, 25]                       // Attack damage values
+    );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
-  };
-  
-  const runMain = async () => {
+};
+
+const runMain = async () => {
     try {
-      await main();
-      process.exit(0);
+        await main();
+        process.exit(0);
     } catch (error) {
-      console.log(error);
-      process.exit(1);
+        console.log(error);
+        process.exit(1);
     }
-  };
-  
-  runMain();
-  
+};
+
+runMain();
