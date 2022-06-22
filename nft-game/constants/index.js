82ef09db2394d89d60fd88ef52a0e62e583c9aba
WizardlyDeveloper <wizardlydeveloper@gmail.com>
2022-06-22 16:59:33 +0500

    setup default starter css home page

---
+++ nft-game/constants/index.js
@@ -0,0 +1,7 @@
+
+export const TWITTER_HANDLE = '_buildspace';
+export const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+export const TWITTER_HANDLE_ZEE = 'zainamroti';
+export const TWITTER_LINK_ZEE = `https://twitter.com/${TWITTER_HANDLE_ZEE}`;
+
